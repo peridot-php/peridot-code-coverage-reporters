b197b96eee419286ca2edb81330e146fbb20cbb5
Austin Morris <austin.morris@gmail.com>
2014-10-16 22:01:32 -0400

    create plugin class for easy reporter registration

---
+++ src/Plugin.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Peridot\CodeCoverage;
+
+use Evenement\EventEmitterInterface;
+use Peridot\Reporter\ReporterFactory;
+use Symfony\Component\Console\Input\InputInterface;
+
+/**
+ * Class Plugin
+ * @package Peridot\CodeCoverage
+ */
+class Plugin
+{
+    public static function load(EventEmitterInterface $eventEmitter)
+    {
+        $eventEmitter->on('peridot.reporters', function(InputInterface $input, ReporterFactory $reporterFactory) {
+            $reporterFactory->register(
+                'clover-code-coverage',
+                'Code coverage with a PHP_CodeCoverage style Clover report',
+                'Peridot\CodeCoverage\Reporter\CloverCodeCoverageReporter'
+            );
+
+            $reporterFactory->register(
+                'crap4j-code-coverage',
+                'Code coverage with a PHP_CodeCoverage style Crap4J report',
+                'Peridot\CodeCoverage\Reporter\Crap4JCodeCoverageReporter'
+            );
+
+            $reporterFactory->register(
+                'html-code-coverage',
+                'Code coverage with a PHP_CodeCoverage style HTML report',
+                'Peridot\CodeCoverage\Reporter\HTMLCodeCoverageReporter'
+            );
+
+            $reporterFactory->register(
+                'php-code-coverage',
+                'Code coverage with a PHP_CodeCoverage style PHP report',
+                'Peridot\CodeCoverage\Reporter\PHPCodeCoverageReporter'
+            );
+
+            $reporterFactory->register(
+                'text-code-coverage',
+                'Code coverage with a PHP_CodeCoverage style Text report',
+                'Peridot\CodeCoverage\Reporter\TextCodeCoverageReporter'
+            );
+
+            $reporterFactory->register(
+                'xml-code-coverage',
+                'Code coverage with a PHP_CodeCoverage style XML report',
+                'Peridot\CodeCoverage\Reporter\XMLCodeCoverageReporter'
+            );
+        });
+    }
+}
